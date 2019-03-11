d0bdc291f5b168988a5c9efb4ac878124677ff62
Grigory <Rygor83@users.noreply.github.com>
2019-03-11 12:46:31 +0300

    0.3: initial2

--- sap_cmd.py
+++ sap_cmd.py
@@ -1,47 +1,53 @@
 import click
 
 
+def print_version(ctx, param, value):
+	if not value or ctx.resilient_parsing:
+		return click.echo('Version 1.0 \n')
+	ctx.exit()
+
+
 @click.group()
-@click.version_option()
+# @click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True, help='Version of script')
 def cli():
 	""" SAP command line """
 
 
 @cli.command('run')
 @click.argument('system')
-@click.argument('mandant', required=False)
+@click.argument('mandant', required=False, type=click.IntRange(1,999))
 @click.option('-u', help='user id')
 @click.option('-p', help='password')
 @click.option('-l', help='language', default='RU')
 @click.option('-v', help='verbose', is_flag=True)
-def run(system, mandant, u='', p='', l = 'RU', v=''):
+def run(system, mandant, u='', p='', l='RU', v=''):
 	""" start SAP system """
 	print(system, mandant, u, p, l, v)
 
 
 @cli.command('db')
 def database():
-	''' create database '''
+	''' Create database '''
 	print('create database')
-	
-	
+
+
 @cli.command('add')
 def add():
-	''' add SAP system to database '''
+	''' Add SAP system to database '''
 	print('adding system to db')
-	
-	
+
+
 @cli.command('ini')
 def ini():
-	''' create ini file '''
+	''' Create ini file '''
 	print('creating ini file')
-	
-	
+
+
 @cli.command('show')
 @click.option('-s')
 @click.option('-all', is_flag=True)
 def show(all, s):
-	''' show available systems in db '''
+	''' Show available systems in db '''
 	if all:
 		print('showing all systems')
 	else:
