496aa112302707fc27dedbe24887c9ee37e6f966
Grigory <Rygor83@users.noreply.github.com>
2019-03-13 00:34:11 +0300

    04. implemented basic sqlalchemy

--- sap_cmd.py
+++ sap_cmd.py
@@ -1,10 +1,28 @@
 import click
+import os
+import sqlalchemy
+from sqlalchemy import Table, Column, Integer, Numeric, String
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy import PrimaryKeyConstraint, UniqueConstraint, CheckConstraint, ForeignKeyConstraint
+from sqlalchemy import ForeignKey, Boolean
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///sap_info.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
 
 
-def print_version(ctx, param, value):
-	if not value or ctx.resilient_parsing:
-		return click.echo('Version 1.0 \n')
-	ctx.exit()
+class Sap(Base):
+	__tablename__ = 'sap'
+	system_id = Column(String(3), primary_key=True)
+	mandant_num = Column(String(3), primary_key=True)
+	user_id = Column(String(10), primary_key=True)
+	password = Column(String(55))
 
 
 @click.group()
@@ -15,26 +33,109 @@ def cli():
 
 @cli.command('run')
 @click.argument('system')
-@click.argument('mandant', required=False, type=click.IntRange(1,999))
+@click.argument('mandant', required=False, type=click.IntRange(1, 999))
 @click.option('-u', help='user id')
 @click.option('-p', help='password')
 @click.option('-l', help='language', default='RU')
 @click.option('-v', help='verbose', is_flag=True)
 def run(system, mandant, u='', p='', l='RU', v=''):
-	""" start SAP system """
-	print(system, mandant, u, p, l, v)
+	""" Start SAP system """
+
+	query = session.query(Sap.system_id, Sap.mandant_num, Sap.user_id,
+																							Sap.password)
+	if system:
+		query = query.filter_by(system_id=system)
+	if mandant:
+		query = query.filter_by(mandant_num=mandant)
+	if u:
+		query = query.filter_by(user_id=u)
+
+	sap_data = query.all()
+	print(sap_data)
 
 
 @cli.command('db')
-def database():
+@click.argument('db_name', default='sap_info')
+def database(db_name):
 	''' Create database '''
-	print('create database')
+
+	# check if db exists
+	file_exists = False
+	for fname in os.listdir('.'):
+		if fname.endswith('.db'):
+			file_exists = True
+
+	if file_exists:
+		print('Database already exists')
+	else:
+		engine = create_engine(f"sqlite:///{db_name}.db")
+		Base.metadata.create_all(engine)
+		print('Database', db_name, 'created')
 
 
 @cli.command('add')
-def add():
+@click.option('-system', prompt=True, help='system id')
+@click.option(
+	'-mandant', prompt=True, help='mandant num', type=click.IntRange(1, 999))
+@click.option('-user', prompt=True, help='user id')
+@click.option(
+	'-password',
+	help='password',
+	prompt=True,
+	confirmation_prompt=True
+	#, hide_input=True
+)
+def add(system, mandant, user, password):
 	''' Add SAP system to database '''
-	print('adding system to db')
+	print('\nadding system to db')
+	print(system, mandant, user, password)
+
+	Base = declarative_base()
+
+	sap_info = Sap(
+		system_id=system, mandant_num=mandant, user_id=user, password=password)
+
+	session.add(sap_info)
+	session.commit()
+
+
+@cli.command('update')
+@click.option('-system', prompt=True, help='system id')
+@click.option(
+	'-mandant', prompt=True, help='mandant num', type=click.IntRange(1, 999))
+@click.option('-user', prompt=True, help='user id')
+@click.option(
+	'-password',
+	help='password',
+	prompt=True,
+	confirmation_prompt=True
+	#, hide_input=True
+)
+def update(system, mandant, user, password):
+	''' Update password for system, mandant and user '''
+
+	query = session.query(Sap)
+	result = query.filter(Sap.system_id == system, Sap.mandant_num == mandant,
+																							Sap.user_id == user).first()
+	result.password = password
+	session.commit()
+
+
+@cli.command('delete')
+@click.option('-system', prompt=True, help='system id')
+@click.option(
+	'-mandant', prompt=True, help='mandant num', type=click.IntRange(1, 999))
+@click.option('-user', prompt=True, help='user id')
+def delete(system, mandant, user):
+	''' Deleting specific system '''
+
+	query = session.query(Sap)
+	result = query.filter(Sap.system_id == system, Sap.mandant_num == mandant,
+																							Sap.user_id == user).one()
+	session.delete(result)
+	session.commit()
+	
+	print('\nSystem', system, '-', mandant, 'deleted succesfully for user', user)
 
 
 @cli.command('ini')
@@ -44,14 +145,22 @@ def ini():
 
 
 @cli.command('show')
-@click.option('-s')
-@click.option('-all', is_flag=True)
+@click.option('-s', required=False)
+@click.option('-all', is_flag=True, required=False)
 def show(all, s):
 	''' Show available systems in db '''
 	if all:
-		print('showing all systems')
-	else:
-		print('showing only system', s)
+		sap_data = session.query(Sap.system_id, Sap.mandant_num, Sap.user_id,
+																											Sap.password).all()
+		for item in sap_data:
+			print(item)
+	elif s:
+		query = session.query(Sap.system_id, Sap.mandant_num, Sap.user_id,
+																								Sap.password)
+		query = query.filter_by(system_id=s)
+		sap_data = query.all()
+		for item in sap_data:
+			print(item)
 
 
 if __name__ == '__main__':
